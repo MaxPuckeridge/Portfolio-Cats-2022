d3ee31e0427c54938f1aec16f411e5e60ea8aac7
Max Puckeridge <max.puckeridge@gmail.com>
2022-09-28 10:08:20 +1000

    Remove some tags that mess with the url

--- lib/fetcher/fetch-tags.ts
+++ lib/fetcher/fetch-tags.ts
@@ -4,7 +4,7 @@ import { fetcher } from './fetcher';
 
 export const fetchAllTags = async () => {
   const set = (await fetcher<string[]>(createTagsAPIEndpoint()))
-    .filter((tag) => !!tag)
+    .filter((tag) => !!tag && !tag.includes('#') && !tag.includes('?'))
     .map((tag) => tag.toLowerCase())
     .reduce((set, value) => {
       set.add(value);
